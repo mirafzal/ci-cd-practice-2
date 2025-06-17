5736751f513b92a8881c54dd32cf48aefb32d63d
= <shavkatovm99@gmail.com>
2025-06-17 20:51:47 +0500

    add go app and ci/cd for it

---
+++ mygoapp/go.mod
@@ -0,0 +1,5 @@
+module github.com/mirafzal/ci-cd-practice-2
+
+go 1.21
+
+require github.com/gin-gonic/gin v1.10.0
---
+++ mygoapp/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestPingRoute(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("Expected status 200, got %d", w.Code)
+	}
+}
